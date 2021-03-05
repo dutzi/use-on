6f0990215b94abcd90d05bfd468132c1c5c3df0b
Eldad Bercovici <dutzi.b@gmail.com>
2021-03-05 22:57:02 +0200

    remove side effects from render (#1)
    * fix wip
    
    * removed side effects from render(?)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react';
-import ResizeComponent from './ResizeComponent';
+import React, { useState } from 'react';
+
+import ListenerComponent from './ListenerComponent';
 import './App.css';
 
 export default function App() {
   const [mount, setMount] = useState(false);
 
   return (
-    <div className="App">
-      <pre>
-        {`
+    <React.StrictMode>
+      <div className="App">
+        <pre>
+          {`
         Steps to reproduce:
 
         1. Open the DevTools' Console
-        2. Click on "Mount Resize Component"
-        3. Resize window
+        2. Click on "Mount Listener Component"
+        3. Press any key...
 
-        - You should see "resize!" printed in the console
+        - You should see "keydown handler!" printed in the console
 
-        4. Click on "Unmount Resize Component"
+        4. Click on "Listen to MouseDown"
+        5. Click anywhere
 
-        - "resize!" messages should stop showing up in the console, but they don't!
+        - You should see "mousedown handler!" printed in the console, but they don't (in production)
       `
-          .replace(/  +/g, '')
-          .trim()}
-      </pre>
-      <div className="gutter">
-        <button onClick={() => setMount(!mount)}>
-          {mount ? 'Unmount Resize Component' : 'Mount Resize Component'}
-        </button>
+            .replace(/  +/g, '')
+            .trim()}
+        </pre>
+        <div className="gutter">
+          <button onClick={() => setMount(!mount)}>
+            {mount ? 'Unmount Listener Component' : 'Mount Listener Component'}
+          </button>
+        </div>
+        {mount && <ListenerComponent />}
       </div>
-      {mount && <ResizeComponent />}
-    </div>
+    </React.StrictMode>
   );
 }
---
+++ src/ListenerComponent.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import useOn from './use-on';
+
+export default function ListenerComponent() {
+  const [eventName, setEventName] = useState('keydown');
+  console.log(eventName);
+  useOn()
+    .who(window)
+    .when(eventName)
+    .what(() => {
+      console.log(`${eventName} handler`);
+    });
+
+  function handleToggleEventName() {
+    setEventName(eventName === 'keydown' ? 'mousedown' : 'keydown');
+  }
+
+  return (
+    <div>
+      <pre style={{ marginBottom: '1em', width: 'auto' }}>
+        Listening to {eventName === 'keydown' ? 'key-down' : 'mouse-down'}...
+      </pre>
+      <div className="gutter">
+        <button onClick={handleToggleEventName}>
+          Listen to {eventName === 'keydown' ? 'MouseDown' : 'Keydown'}
+        </button>
+      </div>
+    </div>
+  );
+}
